c32fbcc70844fda3bf2c97bf13bc6a92450b1932
Erich Woo <erich.woo.23@dartmouth.edu>
2020-10-10 14:32:33 -0700

    loading kernal source file for editing

---
+++ kernel.c
@@ -0,0 +1,10 @@
+/* Erich Woo & Boxian Wang
+ * 10 October 2020
+ * Kernel functionality
+ */
+
+/************ Data Structs *********/
+
+/*********** Function Prototypes *********/
+
+/********** Function Pseudocodes **********/
